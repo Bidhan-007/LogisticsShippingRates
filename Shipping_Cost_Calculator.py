d4a976526c14a3deaf0f925c5f2c053c6a835bc5
Bidhan-007 <ybidhan121@gmail.com>
2026-09-06 02:32:10 -0400

    New Fare Charge included for FragileItems

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,11 +1,17 @@
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
+kindOfItem=input("Is it Fragile Item or Non-Fragile ? : ")
 weight = float(input("Enter the package weight in kilograms: "))
 rate = float(input("Enter the shipping rate per kilogram: "))
 
 ## Calculate shipping cost
 shipping_cost = weight * rate
+
+#Fragile Item Added Cost 
+# per1kg - 0.5USD
+if(kindOfItem=='fragile' | kindOfItem=='FRAGILE' | kindOfItem=='Fragile')
+    shipping_cost+=(weight*0.5)
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
